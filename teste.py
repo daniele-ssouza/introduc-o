7adb424db68c23ec3f660cc89de58a33bc192026
daniele-ssouza <166664849+daniele-ssouza@users.noreply.github.com>
2024-04-16 00:00:20 +0000

    varias alterações

--- teste.py
+++ teste.py
@@ -4,4 +4,3 @@ if x>y:
     print(f"{x} é maior que {y}")
 else:
     print(f"{y} é maior que {x}")
-
---
+++ teste4.py
@@ -0,0 +1,5 @@
+import random
+
+for i in range(1, 101):
+    numero = random.randint(0, 100)
+    lista.append(numero)
